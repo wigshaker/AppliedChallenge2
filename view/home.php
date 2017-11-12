402f8bf200574e839783aae8b1901ba32f9c9298
Clay Caylor <wigshaker@gmail.com>
2017-11-12 14:11:11 -0600

    typo in home.php

--- view/home.php
+++ view/home.php
@@ -3,7 +3,7 @@
     <div role="main" class="ui-content">
         <ul data-role="listview" data-inset="true">
             <li><a href="../view/live_view.php">Live Cam</a></li>
-            <li><a href="../view/util_txt_import.php">Motion Detection Log</a></li>
+            <li><a href="../view/motion_log.php">Motion Detection Log</a></li>
         </ul>
 
     </div>
