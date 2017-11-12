261999bc5ab3a375c5395ab7297cee3b186ed6c2
Clay Caylor <wigshaker@gmail.com>
2017-11-12 14:09:15 -0600

    typos in home.php

--- view/home.php
+++ view/home.php
@@ -1,10 +1,10 @@
-<?php include '..view\header.php'; ?>
+<?php include '..\view\header.php'; ?>
 
     <div role="main" class="ui-content">
         <ul data-role="listview" data-inset="true">
-            <li><a href="..view/live_view.php">Live Cam</a></li>
+            <li><a href="../view/live_view.php">Live Cam</a></li>
             <li><a href="../view/util_txt_import.php">Motion Detection Log</a></li>
         </ul>
 
     </div>
-<?php include '..view\footer.php'; ?>
+<?php include '..\view\footer.php'; ?>
