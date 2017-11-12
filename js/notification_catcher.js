94ecef31f00a612825a56529a800724200cdd6fb
Clay Caylor <wigshaker@gmail.com>
2017-11-12 16:29:32 -0600

    Notification Catcher PHP and JS

---
+++ js/notification_catcher.js
@@ -0,0 +1,8 @@
+if(typeof(EventSource) !== "undefined") {
+    var source = new EventSource("http://wigshaker.ddns.net:41817/html/ac2/model/msg_push.php");
+    source.onmessage = function(event) {
+        document.getElementById("result").innerHTML += event.data + "<br>";
+    };
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+}
