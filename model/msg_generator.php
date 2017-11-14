54a00400d1765e314f16c42b504900cc0d56329c
Clay Caylor <wigshaker@gmail.com>
2017-11-14 14:01:02 -0600

    SSE

--- model/msg_generator.php
+++ model/msg_generator.php
@@ -6,26 +6,26 @@ header('Cache-Control: no-cache');
 $file_path = '../../motionLog.txt';
 $last_time = max(filemtime($file_path), $_SESSION["last_time"]);
 $motion_time = 0;
-//
+
 // while (true) {
 // 	if ($motion_time > $last_time) {
-		echo "data: Most recent log activity: ffffff\n\n";
-		ob_flush();
-		flush();
-   //
-	// 	$last_time = $motion_time;
-	// 	$_SESSION["last_time"] = $motion_time;
-	// } else {
-	// 	$last_time = time();
-	// 	$motion_time = filemtime($file_path);
+// 		echo "data: Most recent log activity: {$motion_time}\n\n";
+// 		ob_flush();
+// 		flush();
+//
+// 		$last_time = $motion_time;
+// 		$_SESSION["last_time"] = $motion_time;
+// 	} else {
+// 		$last_time = time();
+// 		$motion_time = filemtime($file_path);
       //
 		// //FOR TESTING ONLY!!!!!!!!!!!!!!!!!!!
 		// $time = date('r');
 		// echo "data: The server time is: {$time}\n\n";
 		// ob_flush();
 		// flush();
-	}
-
-	sleep(5);  //Poll every 5 secs
-}
+// 	}
+//
+// 	sleep(5);  //Poll every 5 secs
+// }
 ?>
