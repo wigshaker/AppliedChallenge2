e820687ef5e248574cf65f61665a97b792df25a7
Clay Caylor <wigshaker@gmail.com>
2017-11-14 16:54:07 -0600

    SSE

--- model/msg_generator.php
+++ model/msg_generator.php
@@ -17,19 +17,9 @@ while (true) {
 		// $_SESSION["last_time"] = $motion_time;
 		// session_write_close();
 
-	} // else {
+	}
 		// $last_time = time();
 		$motion_time = filemtime($file_path);
-      //
-		// echo "data: {$motion_time}\n\n";
-		// ob_flush();
-		// flush();
-		//FOR TESTING ONLY!!!!!!!!!!!!!!!!!!!
-		// $time = date('r');
-		// echo "data: The server time is: {$time}\n\n";
-		// ob_flush();
-		// flush();
-	// }
 
 	clearstatcache();
 	sleep(5);  //Poll every 5 secs
