a3565d625d489c6705733806ba51e626a22a4dba
Clay Caylor <wigshaker@gmail.com>
2017-11-14 17:38:40 -0600

    js vibration testing

--- js/notification_handler.js
+++ js/notification_handler.js
@@ -2,7 +2,7 @@ if(typeof(EventSource) !== "undefined") {
     var source = new EventSource("http://wigshaker.ddns.net:4/html/ac2/model/msg_generator.php");
     source.onmessage = function(event) {
         document.getElementById("result").innerHTML += event.data + "<br>";
-        navigator.vibrate(150);
+        window.navigator.vibrate(150);
     };
 } else {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
