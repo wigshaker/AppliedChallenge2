f6dbbcd0e6e018a7e1d1b044190da11e933fedea
Clay Caylor <wigshaker@gmail.com>
2017-11-14 13:58:46 -0600

    SSE testing

--- model/msg_generator.php
+++ model/msg_generator.php
@@ -6,19 +6,19 @@ header('Cache-Control: no-cache');
 $file_path = '../../motionLog.txt';
 $last_time = max(filemtime($file_path), $_SESSION["last_time"]);
 $motion_time = 0;
-
-while (true) {
-	if ($motion_time > $last_time) {
+// 
+// while (true) {
+// 	if ($motion_time > $last_time) {
 		echo "data: Most recent log activity: {$motion_time}\n\n";
 		ob_flush();
 		flush();
-
-		$last_time = $motion_time;
-		$_SESSION["last_time"] = $motion_time;
-	} else {
-		$last_time = time();
-		$motion_time = filemtime($file_path);
-      // 
+   //
+	// 	$last_time = $motion_time;
+	// 	$_SESSION["last_time"] = $motion_time;
+	// } else {
+	// 	$last_time = time();
+	// 	$motion_time = filemtime($file_path);
+      //
 		// //FOR TESTING ONLY!!!!!!!!!!!!!!!!!!!
 		// $time = date('r');
 		// echo "data: The server time is: {$time}\n\n";
