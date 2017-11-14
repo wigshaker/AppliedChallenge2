0c043a446a9314794c39a2202f98c015b6e47623
Clay Caylor <wigshaker@gmail.com>
2017-11-14 13:27:21 -0600

    Motion SSE improvement

--- model/msg_generator.php
+++ model/msg_generator.php
@@ -4,8 +4,8 @@ header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
 $file_path = '../../motionLog.txt';
-$last_time = max(filemtime($file_path), $_SESSION('last_time'));
-$motion_time = 0
+$last_time = max(filemtime($file_path), $_SESSION["last_time"]);
+$motion_time = 0;
 
 while (true) {
 	if ($motion_time > $last_time) {
@@ -14,17 +14,18 @@ while (true) {
 		flush();
 
 		$last_time = $motion_time;
-		$_SESSION('last_time') = $motion_time;
+		$_SESSION["last_time"] = $motion_time;
 	} else {
 		$last_time = time();
 		$motion_time = filemtime($file_path);
 
 		//FOR TESTING ONLY!!!!!!!!!!!!!!!!!!!
-		echo "data: no activity.\n\n";
+		$time = date('r');
+		echo "data: The server time is: {$time}\n\n";
 		ob_flush();
 		flush();
 	}
 
-	sleep(10);  //Poll every 10 secs
+	sleep(5);  //Poll every 5 secs
 }
 ?>
