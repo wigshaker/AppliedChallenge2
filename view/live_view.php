c03f5b7cc07a164714cca9c923e87fe126a97d46
Clay Caylor <wigshaker@gmail.com>
2017-11-15 16:20:11 -0600

    cdcca

--- view/live_view.php
+++ view/live_view.php
@@ -3,10 +3,12 @@
    require_once('model/valid_user.php');
  ?>
 
-<div role="main" class="ui-content">
+<script src="js/preview_catcher.js"></script>
+
+<div role="main" class="ui-content"  onload="setTimeout('init();', 100);">
 
     <img id="mjpeg_dest" src="" width="1280" height="720" alt="Monitor Image"/>
 
 </div>
-      
+
 <?php include 'view/footer.php'; ?>
