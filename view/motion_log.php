28127f2c2dcd82474981218ad9bc8f55f59b3827
Clay Caylor <wigshaker@gmail.com>
2017-11-15 16:13:01 -0600

    asdas

--- view/motion_log.php
+++ view/motion_log.php
@@ -19,7 +19,7 @@
 
 <div role="main" class="ui-content">
 
-   <form id="motion_options" action=".?action=show_motion_log" method="post">
+   <form id="motion_options" action="index.php?action=show_motion_log" method="post">
       <input type="hidden" name="notification-enabled" value="true">
       <label for="notification-enabled">Motion notifications:</label>
       <input type="checkbox" data-role="flipswitch" onchange="$('#motion_options').submit()"
