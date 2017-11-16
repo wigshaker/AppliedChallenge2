dd76c22e90ad9b041127723bf20c1e75c0c009cc
Clay Caylor <wigshaker@gmail.com>
2017-11-16 13:50:03 -0600

    dsdsf

--- view/motion_log.php
+++ view/motion_log.php
@@ -15,7 +15,7 @@
       // navigator.vibrate(300);
       // location.reload(true);
       $('#log-list').load(document.URL +  ' #log-list', function() {
-         $('#log-list').listview('refresh');
+         $('#log-list').listview();
          });
    };
    // source.onmessage = setTimeout(function(event)  {
