f13ab1c91d3a84043dcb5978aa2002418f3d665d
Clay Caylor <wigshaker@gmail.com>
2017-11-15 20:43:11 -0600

    reintroducing note.php as header include.

--- view/header.php
+++ view/header.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Clay Caylor | Info2439 | Applied Challenge 2</title>
-    <meta charset="utf-8">
-  	<meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-    <link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css">
-    <link rel="stylesheet" href="css/master.css">
-    <script src="js/jquery-2.1.3.min.js"></script>
-    <script src="js/jqMobile_overrides.js"></script>
-    <script src="js/jquery.mobile-1.4.5.min.js"></script>
+   <head>
+      <title>Clay Caylor | Info2439 | Applied Challenge 2</title>
+      <meta charset="utf-8">
+      <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+      <link rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css">
+      <link rel="stylesheet" href="css/master.css">
+      <script src="js/jquery-2.1.3.min.js"></script>
+      <script src="js/jqMobile_overrides.js"></script>
+      <script src="js/jquery.mobile-1.4.5.min.js"></script>
+      <?php include('model/notification_handler.php'); ?>
 
-    <link rel="icon" type="image/png" href="icons/favicon-196x196.png" sizes="196x196" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="icons/iconsapple-touch-icon-152x152.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="icons/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="icons/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="icons/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="icons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="icons/apple-touch-icon-57x57.png" />
-  </head>
+      <link rel="icon" type="image/png" href="icons/favicon-196x196.png" sizes="196x196" />
+      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="icons/iconsapple-touch-icon-152x152.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="icons/apple-touch-icon-144x144.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="120x120" href="icons/apple-touch-icon-120x120.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="icons/apple-touch-icon-114x114.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="icons/apple-touch-icon-72x72.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="57x57" href="icons/apple-touch-icon-57x57.png" />
+   </head>
 
-  <body>
-  <!--
-    Clay Caylor
-    default.html
-    Info2439.8A
-    Brown
-    10/25/2017
-  -->
-    <div data-role="page">
+   <body>
+   <!--
+   Clay Caylor
+   default.html
+   Info2439.8A
+   Brown
+   10/25/2017
+   -->
+      <div data-role="page">
 
       <div data-role="header" data-position='fixed'>
-        <h1>Remote Child Monitor</h1>
+         <h1>Lamb Cam</h1>
       </div>
