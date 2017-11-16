52d45d99504d6154821103ee2e0cd6c66106293b
Clay Caylor <wigshaker@gmail.com>
2017-11-15 18:20:29 -0600

    moving video init around

--- view/live_view.php
+++ view/live_view.php
@@ -4,8 +4,11 @@
  ?>
 
 <script src="js/preview_catcher.js"></script>
-
-<div role="main" class="ui-content"  onload="setTimeout('init();', 100);">
+<script type="text/javascript">
+   $(document).on("pageinit",".ui-content",setTimeout('init();', 100););
+</script>
+  <!-- onload="setTimeout('init();', 100);" -->
+<div role="main" class="ui-content">
 
     <img id="mjpeg_dest" src="" width="1280" height="720" alt="Monitor Image"/>
 
