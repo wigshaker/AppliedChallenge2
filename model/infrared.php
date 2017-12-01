c8872c97a0af9ef051d9bb6d65886fea640e9fe2
Clay Caylor <wigshaker@gmail.com>
2017-11-30 18:48:17 -0600

    trying system() function to control GPIO

--- model/infrared.php
+++ model/infrared.php
@@ -1,23 +1,23 @@
 <?php
    session_start();
-   require_once 'vendor/autoload.php';
-   use PiPHP\GPIO\GPIO;
-   use PiPHP\GPIO\Pin\OutputPinInterface;
-
-   // Create a GPIO object
-   $gpio = new GPIO();
-
-   // Retrieve pin 4 and configure it as an output pin
-   $ir_pin = $gpio->getOutputPin(4);
+   // require_once 'vendor/autoload.php';
+   // use PiPHP\GPIO\GPIO;
+   // use PiPHP\GPIO\Pin\OutputPinInterface;
+   //
+   // // Create a GPIO object
+   // $gpio = new GPIO();
+   //
+   // // Retrieve pin 4 and configure it as an output pin
+   // $ir_pin = $gpio->getOutputPin(4);
 
    // Turn IR LED pins on or off, for nightvision.
    try {
       if (!isset($_SESSION['ir_enabled']) {
-         $ir_pin->setValue(OutputPinInterface::VALUE_LOW);
+         system ( "gpio -g write 0 0" );
       } else if ($_SESSION['ir_enabled'] == '0') {
-         $ir_pin->setValue(OutputPinInterface::VALUE_LOW);
+         system ( "gpio -g write 0 0" );
       } else if ($_SESSION['ir_enabled'] === '1') {
-         $ir_pin->setValue(OutputPinInterface::VALUE_HIGH);
+         system ( "gpio -g write 0 1" );
       }
    } catch (Exception $e) {
       $error_message = $e->getMessage();
